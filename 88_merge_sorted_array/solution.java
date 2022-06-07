378f738b5efaa70fdf2355dad4f0876f68008941
Mason Kleinsorge <Kleinsorgem97@gmail.com>
2022-06-07 13:46:31 -0500

    challenge 88

---
+++ 88_merge_sorted_array/solution.java
@@ -0,0 +1,25 @@
+/**
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, 
+ * and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+ * 
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ * 
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+ * To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that 
+ * should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ */
+
+class solution {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0) return;
+
+        int r = m + n - 1;
+        m--; n--;
+
+        while (m >= 0 && n >= 0) 
+            nums1[r--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+
+        while (n >= 0)
+            nums1[r--] = nums2[n--];
+    }
+}
