b3962ba231ba2403028a798e7a75ffe8f0e17ac8
Mason Kleinsorge <kleinsorgem97@gmail.com>
2022-06-10 11:10:21 -0500

    problem 3 solution

--- 3_longest_substring_without_repeating_characters/solution.java
+++ 3_longest_substring_without_repeating_characters/solution.java
@@ -2,11 +2,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
-    public static void main(String[] args) {
-        int longest = lengthOfLongestSubstring("dvdf");
-        System.out.println(longest);
-    }
-
     public static int lengthOfLongestSubstring(String s) {   
         int l = 0, result = 0;
         Set<Character> set = new HashSet<Character>();
