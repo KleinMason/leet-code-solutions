648822368f4bd5917c4fbca615f5649ce68e69b0
Mason Kleinsorge <Kleinsorgem97@outlook.com>
2022-06-13 14:42:47 -0500

    problem 120

---
+++ 120. Triangle/solution.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+class solution {
+    /**
+     * Not my solution! See REAME.md
+     */
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int level = n - 1; level >= 0; level--)
+            for (int i = 0; i <= level; i++)
+                dp[level][i] = triangle.get(level).get(i) + Math.min(dp[level + 1][i], dp[level + 1][i + 1]);
+
+        return dp[0][0];
+    }
+}
